5c17e48acd0f2c3751e4a35d61be6f325126a195
Pictomate <hello@pictomate.com>
2025-03-26 21:13:21 +0100

    Copy thing removed

--- plugin/webpack.config.js
+++ plugin/webpack.config.js
@@ -1,6 +1,24 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+
+/**
+ * ARCHITEKTURA PROJEKTU
+ * --------------------
+ * assets/ - katalog z ikonami i metadanymi, hostowany na Vercel
+ *   ├─ icons/ - folder zawierający pliki SVG ikon
+ *   └─ icons-metadata.json - plik z metadanymi ikon, generowany przez generate-icons.js
+ * 
+ * plugin/ - kod źródłowy pluginu Figma
+ *   ├─ dist/ - skompilowany plugin (nie zawiera ikon)
+ *   └─ src/ - kod źródłowy pluginu
+ * 
+ * PRZEPŁYW DANYCH
+ * --------------
+ * 1. generate-icons.js skanuje katalog assets/icons/ i generuje plik assets/icons-metadata.json
+ * 2. Plik assets/icons-metadata.json jest hostowany na Vercel razem z katalogiem icons
+ * 3. Plugin Figma pobiera ikony z zewnętrznego URL (np. https://moje-adres-vercel.app/icons/...)
+ * 4. Ikony NIE są częścią samego pluginu, co pozwala na ich aktualizację bez konieczności aktualizacji pluginu
+ */
 
 module.exports = {
   mode: 'development',
@@ -59,32 +77,10 @@ module.exports = {
       inject: false
     }),
     
-    // Kopiuje pliki ikon i metadanych do katalogu dist
-    // Dzięki temu pliki będą dostępne pod adresem URL: https://twoja-aplikacja.vercel.app/icons/...
-    new CopyPlugin({
-      patterns: [
-        // Kopiuje cały katalog icons do dist/icons
-        { 
-          from: 'icons',
-          to: 'icons',
-          globOptions: {
-            ignore: ['**/.DS_Store', '**/.gitkeep']  // Ignoruje pliki systemowe
-          }
-        },
-        // Kopiuje plik icons-metadata.json do katalogu dist
-        { 
-          from: 'icons-metadata.json', 
-          to: 'icons-metadata.json',
-          noErrorOnMissing: true 
-        },
-        // Kopiuje również plik icons-metadata.json do katalogu src dla użycia podczas developmentu
-        { 
-          from: 'icons-metadata.json', 
-          to: 'src/icons-metadata.json',
-          noErrorOnMissing: true 
-        }
-      ],
-    }),
+    // UWAGA: Usunięto CopyWebpackPlugin
+    // Ikony i metadane są teraz hostowane oddzielnie w katalogu assets/ na Vercel
+    // Plugin pobiera je z zewnętrznego URL, np. https://moje-adres-vercel.app/icons/...
+    // Dzięki temu ikony można aktualizować niezależnie od pluginu
   ],
   
   // Wyłącza minimalizację kodu dla lepszej czytelności
